8a4d33e3c1e9e4b4424be5758e2e009c397ad52e
incon-science <1conscienceofficiel@gmail.com>
2026-06-16 16:35:50 +0200

    ds

--- src/platforms/spike_128.gd
+++ src/platforms/spike_128.gd
@@ -1,4 +1,5 @@
 extends AnimatableBody2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
@@ -16,3 +17,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 			body.play_death_anim()
 			body.dmgshaketimer.start()
 			audio_stream_player_2d.play()
+			
+			sprite_2d.material.set_shader_parameter("hit_effect", 1)
+			await get_tree().create_timer(1).timeout
+			sprite_2d.material.set_shader_parameter("hit_effect", 0)
