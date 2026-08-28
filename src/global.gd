1db6e99dc5ecbd4de60c708e08176112bb674f28
incon-science <1conscienceofficiel@gmail.com>
2026-08-28 23:53:08 +0200

    bvc

--- src/global.gd
+++ src/global.gd
@@ -1,22 +1,25 @@
 extends Node
 
-var debug_mod:bool=true
 
 
-
-var dash_unlock:bool = false
-var sprint_unlock:bool = false
-var doublejump_unlock:bool = false
-var walljump_unlock:bool=true
 
 var godmode:bool=false
 
+var dash_unlock:bool = true
+var doublejump_unlock:bool = false
+var walljump_unlock:bool=true
+
+var sprint_unlock:bool = true
+
+
 var next_door_id:int=3#-1
 var list_door=[
-	{ "door_id": 0, "opened": true, "lvl_name":"start", "pos": Vector2(-1952,1216), "flip_h": true, "door_id_target": "1" },
+	{ "door_id": 0, "opened": false, "lvl_name":"start", "pos": Vector2(-1344.0,1216), "flip_h": true, "door_id_target": "1" },
 	{ "door_id": 1, "opened": true, "lvl_name":"26", "pos": Vector2(-768.0,1216), "flip_h": false, "door_id_target": "0" },
-	{ "door_id": 2, "opened": false, "lvl_name":"26", "pos": Vector2(2368.0,1856), "flip_h": true, "door_id_target": "3" },
+	{ "door_id": 2, "opened": false, "lvl_name":"26", "pos": Vector2(1792.0,1856), "flip_h": true, "door_id_target": "3" },
 	{ "door_id": 3, "opened": true, "lvl_name":"27", "pos": Vector2(-3200,1216.0), "flip_h": false, "door_id_target": "2" },
+	{ "door_id": 4, "opened": true, "lvl_name":"27", "pos": Vector2(-128.0,3776.0), "flip_h": false, "door_id_target": "5" },
+	{ "door_id": 5, "opened": true, "lvl_name":"27", "pos": Vector2(2496.0,1152.0), "flip_h": true, "door_id_target": "4" },
 ]
 
 var color_h_init:float
@@ -43,11 +46,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("escape"):
 		get_tree().quit()
-	if debug_mod:
-		dash_unlock = true
-		sprint_unlock = false
-		doublejump_unlock = false
-		walljump_unlock = true
 		
 	if Input.is_action_just_pressed("start"):
 		get_tree().reload_current_scene()
