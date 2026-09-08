688aae3814a840923307aeecc3a22b8b6f5068bf
incon-science <1conscienceofficiel@gmail.com>
2026-09-08 15:15:40 +0200

    fd

--- src/elements/door.gd
+++ src/elements/door.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var static_body_2d: StaticBody2D = $StaticBody2D
 @onready var cam: PhantomCamera2D = $"../cam"
 
+
 @export var door_id:int
 
 # Called when the node enters the scene tree for the first time.
@@ -13,6 +14,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+		
+		
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -28,21 +31,25 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		var tween = get_tree().create_tween()
 		tween.tween_property(static_body_2d, "global_position:y", static_body_2d.global_position.y-300, 5)
 
-
 func _on_triggernextlvl_body_entered(body: Node2D) -> void:
+	
 	Fadetoblack.transition(7)
 	await Fadetoblack.on_transition_finished
 	
 	Global["next_door_id"]=(Global.list_door[door_id])["door_id_target"]
 	var next_lvl_name = (Global.list_door[Global["next_door_id"]])["lvl_name"]
+	
+	
 	get_tree().change_scene_to_file("res://src/network/scenes/"+next_lvl_name+".tscn")
 
-
+#WARNINGG : NE PAS POSITIONNER LE PERSO SUR UNE ZONE PORTE CAM LIM DANS LEDITEUR ! sinon il entre dans cette zone en premier avant de changer de zone porte
 func _on_limit_cam_zone_body_entered(body: Node2D) -> void:
 	if (Global.list_door[door_id])["flip_h"]:
 		cam.set_limit_right((Global.list_door[door_id])["pos"].x+128)
+		cam.set_limit_left(-10000)
 	else:
 		cam.set_limit_left((Global.list_door[door_id])["pos"].x-128)
+		cam.set_limit_right(10000)
 
 
 func _on_limit_cam_zone_body_exited(body: Node2D) -> void:
--- src/global.gd
+++ src/global.gd
@@ -16,8 +16,8 @@ var list_door=[
 	{ "door_id": 0, "opened": false, "lvl_name":"start", "pos": Vector2(-1344.0,1216), "flip_h": true, "door_id_target": "1" },
 	{ "door_id": 1, "opened": true, "lvl_name":"26", "pos": Vector2(-768.0,1216), "flip_h": false, "door_id_target": "0" },
 	{ "door_id": 2, "opened": false, "lvl_name":"26", "pos": Vector2(1792.0,1920.0), "flip_h": true, "door_id_target": "3" },
-	{ "door_id": 3, "opened": true, "lvl_name":"27", "pos": Vector2(-3200,1088.0), "flip_h": false, "door_id_target": "2" },
-	{ "door_id": 4, "opened": true, "lvl_name":"27", "pos": Vector2(-4800.0,5120.0), "flip_h": false, "door_id_target": "5" },
+	{ "door_id": 3, "opened": true, "lvl_name":"27", "pos": Vector2(-3200,1024.0), "flip_h": false, "door_id_target": "2" },
+	{ "door_id": 4, "opened": true, "lvl_name":"27", "pos": Vector2(-5248.0,5120.0), "flip_h": false, "door_id_target": "5" },
 	{ "door_id": 5, "opened": true, "lvl_name":"27", "pos": Vector2(1920.0,1152.0), "flip_h": true, "door_id_target": "4" },
 ]
 var list_key=[
